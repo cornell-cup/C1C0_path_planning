e13d427e6e491418511e0896242a3dea74c55165
Hokssey Su <xs232@cornell.edu>
2019-11-03 10:40:59 -0500

    finish implementing the draft of heap but not test yet

--- fa19/Search.py
+++ fa19/Search.py
@@ -1,35 +1,164 @@
 
 
 class Heap:
-    def __init__(self, comparator = None):
-        self.heap = [] # list of (comparator(element), element) tuples
+    def __init__(self, comparator = None, isMinHeap):
+        self.KV = [] # list of (tile, key) tuples
         self.mappings = {} # elts -> position in heap
+        self.isMinHeap = isMinHeap
+        self.size = size
         if not self.comparator:
             self.comparator = lambda x: x
         else:
             self.comparator = comparator
 
+    # Constructor for tiles
     def __element__(self, key, value):
         self.key = key
-        self.value = value
+        self.val = value
+    '''
+    If a value with key k1 should be above a value with key
+        k2 in the heap, return 1.
+    If key k1 and key k2 are the same, return 0.
+    If a value with key k1 should be below a value with key
+        k2 in the heap, return -1.
+    This is based on what kind of a heap this is,
+    E.g. a min-heap, the value with the smalllest key is in the root.
+    E.g. a max-heap, the value with the largest key is in the root.
+    '''
+    def comparator(k1, k2):
+        if k1 == k2: return 0
+        if isMinHeap:
+            if k1 < k2:
+                return 1
+            else:
+                return -1
+        else:
+            if k1 > k2:
+                return 1
+            else:
+                return -1
 
-    def __len__(self):
-        return len(self.heap)
+    def compareTo(m, n):
+        return comparator(KV[m].key, KV[n].key)
+
+    def swap(self, index1, index2):
+        assert 0 <= index1 < size and 0 <= index2 < size
+        temp = KV[index1]
+        mappings.update(KV[index2].val = index1)
+        mappings.update(temp.val = index2)
+        KV[index1] = KV[index2]
+        KV[index2] = temp
+
+    def size():
+        return size
 
     def mem(self, val):
         return val in self.mappings
 
-    def bubble_up(self):
-        # TODO
+    '''
+    If KV[n] doesn't exist or has no child, return n.
+        If KV[n] has one child, return its index.
+        If KV[n] has two children with the same priority, return the
+            index of the right one.
+        If KV[n] has two children with different priorities return the
+            index of the one that must appear above the other in a heap.
+    '''
+    def upperChild(n):
+        if n >= size || 2*n + 1 >= size:
+            return n
+        if 2*n + 2 == size(self):
+            return 2*n + 1
+        if compareTo(2*n+1, 2*n+2) == 0:
+            return 2*n + 2
+        if compareTo(2*n+1, 2*n+2) == 1:
+            return 2*n + 1
+        return 2*n + 2
 
-    def bubble_down(self):
+   '''
+   Bubble KV[k] up the heap to its right place.
+     Precondition: 0 <= k < size and
+     The class invariant is true, except perhaps
+     that KV[k] belongs above its parent (if k > 0)
+     in the heap, not below it.
+   '''
+    def bubble_up(k):
         # TODO
-        return
-    def push(self, val):
+        assert 0 <= k < size
+        p = (k - 1) / 2
+        while k > 0 &&  compareTo(KV[k].key, KV[p].key) > 0:
+            swap(k, p)
+            k = p
+            p = ( k-1 ) / 2
+
+    '''
+    Bubble KV[k] down in heap until it finds the right place.
+        If there is a choice to bubble down to both the left and
+        right children (because their priorities are equal), choose
+        the right child.
+        Precondition: 0 <= k < size   and
+                 Class invariant is true except that perhaps
+                 KV[k] belongs below one or both of its children.
+    '''
+    def bubble_down(k):
+        assert 0 <= k < size
+        child = upperChild(k)
+        while c < size && compareTo(k, c) < 0:
+            swap(k, c)
+            k = c
+            c = upperChild(k)
+
+    '''
+    If this is a min-heap, return the heap value with lowest priority.
+    If this is a max-heap, return the heap value with highest priority
+    Do not change the heap. This operation takes constant time.
+    Throw a NoSuchElementException if the heap is empty.
+    '''
+    def peek():
+        if len(KV) == 0:
+            raise NoSuchElementException
+        else:
+            return KV[0].val
+
+    '''
+
+    '''
+    def push(value, key):
         # # TODO:
+        KV[size] = element(value, key)
+        mappings[value] = size
+        size = size + 1
+        bubble_up(size - 1)
 
-    def update_key(self, key):
-        return self
+    '''
+    If this is a min-heap, remove and return heap value with smallest key.
+       If this is a max-heap, remove and return heap value with largest key.
+       The expected time is logarithmic and the worst-case time is linear
+       in the size of the heap.
+       Throw a NoSuchElementException if the heap is empty.
+    '''
+    def poll():
+        if size == 0:
+            raise NoSuchElementException
+        remove = KV[0]
+        swap(0, size - 1)
+        del mappings[KV[size - 1].val]
+        size = size - 1
+        if size > 0:
+            bubble_down(0)
+        return remove.val
+
+
+    '''
+    Change the key of value v to new_key.
+       The expected time is logarithmic and the worst-case time is linear
+       in the size of the heap.
+       Throw an IllegalArgumentException if v is not in the heap. */
+    '''
+    def update_key(value, new_key):
+        index = mappings[value]
+        KV[index].key = new_key
+        bubble_up(index)
+        bubble_down(index)
 
 
 class Grid:
