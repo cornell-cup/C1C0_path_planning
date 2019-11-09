a0a8e23397a7b97aed88adc136658d350c213701
cp456 <cp456@cornell.edu>
2019-11-09 13:15:23 -0500

    heap bug fixes

--- fa19/Search.py
+++ fa19/Search.py
@@ -1,20 +1,19 @@
-
+from collections import namedtuple
+class NoSuchElementException(Exception):
+    pass
 
 class Heap:
+    class Element:
+        def __init__(self, key, value):
+            self.key = key
+            self.val = value
+
     def __init__(self, isMinHeap):
         self.KV = [] # list of (tile, key) tuples
         self.mappings = {} # elts -> position in heap
         self.isMinHeap = isMinHeap
-        self.size = size
-        # if not self.comparator:
-        #     self.comparator = lambda x: x
-        # else:
-        #     self.comparator = comparator
+        self.size = 0
 
-    # Constructor for tiles
-    def __element__(self, key, value):
-        self.key = key
-        self.val = value
     '''
     If a value with key k1 should be above a value with key
         k2 in the heap, return 1.
@@ -25,9 +24,9 @@ class Heap:
     E.g. a min-heap, the value with the smalllest key is in the root.
     E.g. a max-heap, the value with the largest key is in the root.
     '''
-    def comparator(k1, k2):
+    def comparator(self, k1, k2):
         if k1 == k2: return 0
-        if isMinHeap:
+        if self.isMinHeap:
             if k1 < k2:
                 return 1
             else:
@@ -38,19 +37,19 @@ class Heap:
             else:
                 return -1
 
-    def compareTo(m, n):
-        return comparator(KV[m].key, KV[n].key)
+    def compareTo(self, m, n):
+        return self.comparator(self.KV[m].key, self.KV[n].key)
 
     def swap(self, index1, index2):
-        assert 0 <= index1 < size and 0 <= index2 < size
-        temp = KV[index1]
-        mappings.update(KV[index2].val = index1)
-        mappings.update(temp.val = index2)
-        KV[index1] = KV[index2]
-        KV[index2] = temp
+        assert 0 <= index1 < self.size and 0 <= index2 < self.size
+        temp = self.KV[index1]
+        self.mappings[self.KV[index2].val] = index1
+        self.mappings[temp.val] = index2
+        self.KV[index1] = self.KV[index2]
+        self.KV[index2] = temp
 
-    def size():
-        return size
+    def size(self):
+        return self.size
 
     def mem(self, val):
         return val in self.mappings
@@ -63,32 +62,32 @@ class Heap:
         If KV[n] has two children with different keys return the
             index of the one that must appear above the other in a heap.
     '''
-    def upperChild(n):
-        if n >= size || 2*n + 1 >= size:
+    def upperChild(self, n):
+        if n >= self.size or 2 * n + 1 >= self.size:
             return n
-        if 2*n + 2 == size:
+        if 2*n + 2 == self.size:
             return 2*n + 1
-        if compareTo(2*n+1, 2*n+2) == 0:
+        if self.compareTo(2*n+1, 2*n+2) == 0:
             return 2*n + 2
-        if compareTo(2*n+1, 2*n+2) == 1:
-            return 2*n + 1
+        if self.compareTo(2 * n + 1, 2 * n + 2) == 1:
+            return 2 * n + 1
         return 2*n + 2
 
-   '''
+    '''
    Bubble KV[k] up the heap to its right place.
      Precondition: 0 <= k < size and
      The class invariant is true, except perhaps
      that KV[k] belongs above its parent (if k > 0)
      in the heap, not below it.
    '''
-    def bubble_up(k):
+    def bubble_up(self, k):
         # TODO
-        assert 0 <= k < size
+        assert 0 <= k < self.size
         p = (k - 1) / 2
-        while k > 0 &&  compareTo(KV[k].key, KV[p].key) > 0:
-            swap(k, p)
+        while k > 0 and self.compareTo(self.KV[k].key, self.KV[p].key) > 0:
+            self.swap(k, p)
             k = p
-            p = ( k-1 ) / 2
+            p = (k-1) / 2
 
     '''
     Bubble KV[k] down in heap until it finds the right place.
@@ -99,13 +98,13 @@ class Heap:
                  Class invariant is true except that perhaps
                  KV[k] belongs below one or both of its children.
     '''
-    def bubble_down(k):
-        assert 0 <= k < size
-        child = upperChild(k)
-        while c < size && compareTo(k, c) < 0:
-            swap(k, c)
+    def bubble_down(self, k):
+        assert 0 <= k < self.size
+        c = self.upperChild(k)
+        while c < self.size and self.compareTo(k, c) < 0:
+            self.swap(k, c)
             k = c
-            c = upperChild(k)
+            c = self.upperChild(k)
 
     '''
     If this is a min-heap, return the heap value with lowest priority.
@@ -113,21 +112,21 @@ class Heap:
     Do not change the heap. This operation takes constant time.
     Throw a NoSuchElementException if the heap is empty.
     '''
-    def peek():
-        if len(KV) == 0:
+
+    def peek(self):
+        if len(self.KV) == 0:
             raise NoSuchElementException
         else:
-            return KV[0].val
+            return self.KV[0].val
 
     '''
-
+    TODO
     '''
-    def push(value, key):
-        # # TODO:
-        KV[size] = element(value, key)
-        mappings[value] = size
-        size = size + 1
-        bubble_up(size - 1)
+    def push(self, value, key):
+        self.KV[self.size] = self.Element(value, key)
+        self.mappings[value] = self.size
+        self.size = self.size + 1
+        self.bubble_up(self.size - 1)
 
     '''
     If this is a min-heap, remove and return heap value with smallest key.
@@ -136,15 +135,15 @@ class Heap:
        in the size of the heap.
        Throw a NoSuchElementException if the heap is empty.
     '''
-    def poll():
-        if size == 0:
+    def poll(self):
+        if self.size == 0:
             raise NoSuchElementException
-        remove = KV[0]
-        swap(0, size - 1)
-        del mappings[KV[size - 1].val]
-        size = size - 1
-        if size > 0:
-            bubble_down(0)
+        remove = self.KV[0]
+        self.swap(0, self.size - 1)
+        del self.mappings[self.KV[self.size - 1].val]
+        self.size = self.size - 1
+        if self.size > 0:
+            self.bubble_down(0)
         return remove.val
 
 
@@ -154,11 +153,11 @@ class Heap:
        in the size of the heap.
        Throw an IllegalArgumentException if v is not in the heap. */
     '''
-    def update_key(value, new_key):
-        index = mappings[value]
-        KV[index].key = new_key
-        bubble_up(index)
-        bubble_down(index)
+    def update_key(self, value, new_key):
+        index = self.mappings[value]
+        self.KV[index].key = new_key
+        self.bubble_up(index)
+        self.bubble_down(index)
 
 
 class Grid:
@@ -191,47 +190,43 @@ class Grid:
         start.H = self.manhattan_distance(start, goal)
         open_set.add(start)
 
-
         # while heap is not empty
-            # check if top of heap is goal
-            # if not, add to closed set, get empty neighbors and calculate cost and heuristic
-            # if neighbor in open set check if cost is less, if it is, update cost of successor and parent tile
-            # if in closed set and cost is less, put closed set node back on open set and update parent (unsure)
-            while not heap.isEmpty():
-                top_elt = heap.pop()
-                open_set.remove(top_elt)
-                closed_set.add(top_elt)
+        # check if top of heap is goal
+        # if not, add to closed set, get empty neighbors and calculate cost and heuristic
+        # if neighbor in open set check if cost is less, if it is, update cost of successor and parent tile
+        # if in closed set and cost is less, put closed set node back on open set and update parent (unsure)
+        while not heap.isEmpty():
+            top_elt = heap.pop()
+            open_set.remove(top_elt)
+            closed_set.add(top_elt)
 
-                # if the top element is goal, stop the searching algorithm
-                if top_elt == goal:
-                    break
+            # if the top element is goal, stop the searching algorithm
+            if top_elt == goal:
+                break
 
-                # get a list of empty neighbor tiles
-                neighbors = []
-                cur_x = top_elt.x
-                cur_y = top_elt.y
+            # get a list of empty neighbor tiles
+            neighbors = []
+            cur_x = top_elt.x
+            cur_y = top_elt.y
 
-                next_tiles = [(cur_x - 1, cur_y), (cur_x + 1, cur_y), (cur_x, cur_y - 1), (cur_x, cur_y + 1)]
-                for x, y in next_tiles:
-                    if(0 <= x < Grid.rows and 0 <= y < Grid.columns and Grid[x][y].isEmpty and Grid[x][y] not in closed_set):
-                        neighbors.append(Grid[x][y])
+            next_tiles = [(cur_x - 1, cur_y), (cur_x + 1, cur_y), (cur_x, cur_y - 1), (cur_x, cur_y + 1)]
+            for x, y in next_tiles:
+                if(0 <= x < Grid.rows and 0 <= y < Grid.columns and Grid[x][y].isEmpty and Grid[x][y] not in closed_set):
+                    neighbors.append(Grid[x][y])
 
-                # update the cost of successor and parent tile if neighbor
-                # node is in open set
-                for node in neighbors:
-                    if node in open_set:
-                        if top_elt.G + 1 < node.G:
-                            node.G = top_elt.G + 1
-                            node.parent = top_elt
-                    else:
+            # update the cost of successor and parent tile if neighbor
+            # node is in open set
+            for node in neighbors:
+                if node in open_set:
+                    if top_elt.G + 1 < node.G:
                         node.G = top_elt.G + 1
-                        node.H = self.manhattan_distance(node, goal)
                         node.parent = top_elt
-                        open_set.add(node)
-                        heap.push(node)
-
-
-
+                else:
+                    node.G = top_elt.G + 1
+                    node.H = self.manhattan_distance(node, goal)
+                    node.parent = top_elt
+                    open_set.add(node)
+                    heap.push(node)
 
         # fail if get to here here
 
@@ -271,4 +266,4 @@ for i in range(a.rows):
 
 # driver function
 if __name__ == '__main__':
-    main()
+    pass
