70945475f9e24a22ae9fdda390637ae1a1f383f0
cp456 <cp456@cornell.edu>
2019-12-05 17:35:35 -0500

    pose estimation fixes

--- fa19/pose_test.py
+++ fa19/pose_test.py
@@ -1,7 +1,7 @@
 from swatbotics.python import apriltag as sb
-import os
 import cv2
-import collections
+import os
+import glob
 
 # detector options for tag36h11 family
 options = sb.DetectorOptions(families='tag36h11',
@@ -17,14 +17,101 @@ options = sb.DetectorOptions(families='tag36h11',
 
 detector = sb.Detector(options)
 
+"""
+returns a list of all the files in directory [path] with .png or .jpg extension
+assumes: [path] is a string of an absolute path
+"""
+
+
+def getImgsFromDir(path):
+    files = []
+    for ext in ('*.png', '*.jpg'):
+        files.extend(glob.glob(os.path.join(path, ext)))
+
+    return files
+
+
+"""
+captures frames from video of device with device index [device_index] (0 by default)
+when space bar is pressed. Saves captured frames to working directory under name
+[prefix] + frame count. Press 'q' to stop the stream.
+
+assumes [prefix] is a string
+"""
+
+
+def frames_from_video(prefix, device_index=0):
+    video = cv2.VideoCapture(device_index)
+    count = 0
+    while True:
+        check, frame = video.read()
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('stream', img)
+
+        key = cv2.waitKey(1)
+        if key == 32:
+            cv2.imwrite(prefix + str(count) + '.jpg', img)
+            count += 1
+
+        elif key == ord('q'):
+            break
+
+
+"""
+calculates pose from image [image] and prints results from terminal.
+assumes: [image] is an image loaded from cv2.imread() or captured from cv2
+VideoCapture object. [params] is a tuple of intrinsic camera parameters which
+can be calculated from calibrate_camera.py. [tag_size] is the size of the apriltag
+in user defined units, used to scale the results (1.0 by default).
+"""
+
+
+def calculate_pose(image, params, tag_size=1.0):
+    try:
+        detections = detector.detect(image)
+        for i, detection in enumerate(detections):
+            pose, init_err, final_err = detector.detection_pose(
+                detection, params, tag_size)
+            coords = [row[3] for row in pose[:-1]]
+            print('detection {}'.format(i+1))
+            print()
+            print('x = {}'.format(coords[0]))
+            print('y = {}'.format(coords[1]))
+            print('z = {}'.format(coords[2]))
+            print()
+            print('initial error: {}'.format(init_err))
+            print('final error: {}'.format(final_err))
+            print()
+
+    except Exception as e:
+        # print(e)
+        pass
+
+
+"""
+Calculates pose for images in [images] and prints results to terminal, if an image
+in [images] doesn't contain an april tag then prints nothing to terminal for that
+image.
+Assumes: [images] is a list of .png or .jpg files, [params] is a tuple of
+intrinsic camera parameters which can be calculated from calibrate_camera.py,
+and [tag_size] is the size of the apriltag in user defined units,
+used to scale the results (1.0 by default)
+"""
+
+
+def pose_from_imgs(images, params, tag_size=1.0):
+    for image in images:
+        img = cv2.imread(image)
+        calculate_pose(img, params, tag_size)
+
 
 """
 Calculates pose estimation of all apriltags in frame and prints results to the terminal.
-[params] is a tuple of intrinsic camera parameters which can be calculated from 
+[params] is a tuple of intrinsic camera parameters which can be calculated from
 calibrate_camera.py. [tag_size] is the size of the apriltag in user defined units,
-used to scale the results (1.0 by default). 
+used to scale the results (1.0 by default).
 [device_index] is the device index of the camera being used
-for apriltag detection, is 0 (webcam) by default. 
+for apriltag detection, is 0 (webcam) by default.
 
 assumes: [tag_size] > 0 and using tag36h11 tag family
 Note: press q to stop the video stream
@@ -32,35 +119,15 @@ Note: press q to stop the video stream
 
 
 def main(params, tag_size=1.0, device_index=0):
-    video = cv2.VideoCapture(0)
+    video = cv2.VideoCapture(device_index)
 
     while True:
         check, frame = video.read()
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         cv2.imshow('test', img)
-
-        try:
-            detections = detector.detect(img)
-            for i, detection in enumerate(detections):
-                pose, init_err, final_err = detector.detection_pose(
-                    detection, params, tag_size)
-                coords = [row[3] for row in pose[:-1]]
-                print('detection {}'.format(i+1))
-                print()
-                print('x = {}'.format(coords[0]))
-                print('y = {}'.format(coords[1]))
-                print('z = {}'.format(coords[2]))
-                print()
-                print('initial error: {}'.format(init_err))
-                print('final error: {}'.format(final_err))
-                print()
-
-        except Exception as e:
-            # print(e)
-            pass
+        calculate_pose(img, params, tag_size)
 
         key = cv2.waitKey(1)
-
         if key == ord('q'):
             break
 
@@ -71,4 +138,4 @@ def main(params, tag_size=1.0, device_index=0):
 # TODO stopping condition
 if __name__ == '__main__':
     main((1059.96973328701, 1069.2489339794608,
-          622.030969436662, 362.4945777620182))
+          622.030969436662, 362.4945777620182), 6.25)
