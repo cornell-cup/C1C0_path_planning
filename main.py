564b0af16e7db9176a82a79d7366e919fe13ff38
Xueshi Su <xs232@cornell.edu>
2020-02-15 14:36:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # TODO main script
+import grid
+import numpy as np
 
 """
 1) create grid
@@ -8,3 +10,13 @@
 5) Plan path in grid
 6) repeat 3 - 5 until reach goal
 """
+
+class main(args=None):
+  
+  def __init__(self):
+    self.grid = Grid(rows, cols, length)
+    self.x = 0
+    self.y = 0    
+  
+  def update(self):
+    
