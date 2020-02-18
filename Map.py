47c1c61fe2cb35f2d8bac33a2256ab65d86568d1
zcc6 <zcc6@cornell.edu>
2020-02-17 20:10:36 -0500

    Update Map.py

--- Map.py
+++ Map.py
@@ -3,14 +3,15 @@ from time import sleep
 import math
 import grid
 
-FREQUENCY= .5
+FREQUENCY = .5
 
-class GPSThread(threading.Thread):
+
+class MapThread(threading.Thread):
     def __init__(self, threadID):
         threading.Thread.__init__(self)
         self.threadID = threadID
-        self.active = True 
-        self.grid = grid.Grid(500,500,.2)
+        self.active = True
+        self.grid = grid.Grid(500, 500, .2)
         self.sensorDataTop = []
         self.sensorDataBot = []
         self.x = 250
@@ -19,13 +20,14 @@ class GPSThread(threading.Thread):
     def run(self):
         while self.active:
             sleep(FREQUENCY)
-            self.grid.updateGrid(self.x,self.y,self.sensorDataBot,self.sensorDataTop)
+            self.grid.updateGrid(
+                self.x, self.y, self.sensorDataBot, self.sensorDataTop)
 
-    def update(self,sensorDataTop,sensorDataBot,x,y):
+    def update(self, sensorDataTop, sensorDataBot, x, y):
         self.sensorDataTop = sensorDataTop
         self.sensorDataBot = sensorDataBot
         self.x = x
         self.y = y
-      
+
     def kill(self):
         self.active = False
