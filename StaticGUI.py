be99f04c9a65f3d638307b1b0f0677fcdfbac023
Maggie <jg849@cornell.edu>
2020-09-20 19:08:04 -0400

    Change previous 5 paths with green gradual gradient

--- StaticGUI.py
+++ StaticGUI.py
@@ -155,11 +155,47 @@ class SmoothedPathGUI(MapPathGUI):
     def __init__(self, master, inputMap, path):
         super().__init__(master, inputMap, path)
         self.prev_line_id = []
+        self.set_of_prev_path = []
 
     def drawPath(self):
-        # delete previous set of line id if they exist
-        for id in self.prev_line_id:
-            self.canvas.delete(id)
+        # change previous 5 paths with green gradual gradient
+
+        color = '#6eff00'
+        if self.prev_line_id:
+            self.set_of_prev_path.append(self.prev_line_id)
+
+        if self.set_of_prev_path:
+            if self.prev_line_id == self.set_of_prev_path[0]:
+                color = '#347800'
+            elif self.prev_line_id == self.set_of_prev_path[1]:
+                color = '#347800'
+            elif self.prev_line_id == self.set_of_prev_path[2]:
+                color = '#48a600'
+            elif self.prev_line_id == self.set_of_prev_path[3]:
+                color = '#54c200'
+            elif self.prev_line_id == self.set_of_prev_path[4]:
+                color = '#60de00'
+            else:
+                color = 'none'
+
+        if color == 'none':
+            for fst_id in self.set_of_prev_path[0]:
+                self.canvas.delete(fst_id)
+            self.set_of_prev_path.pop(0)
+            color = '#6eff00'
+
+        if len(self.set_of_prev_path) > 4:
+            for id in self.set_of_prev_path[0]:
+                self.canvas.itemconfig(id, fill='#347800')
+            for id in self.set_of_prev_path[1]:
+                self.canvas.itemconfig(id, fill='#347800')
+            for id in self.set_of_prev_path[2]:
+                self.canvas.itemconfig(id, fill='#48a600')
+            for id in self.set_of_prev_path[3]:
+                self.canvas.itemconfig(id, fill='#54c200')
+            for id in self.set_of_prev_path[4]:
+                self.canvas.itemconfig(id, fill='#60de00')
+
         self.prev_line_id = []
 
         idx = 1
@@ -168,10 +204,9 @@ class SmoothedPathGUI(MapPathGUI):
             y1 = self.path[idx - 1].y / tile_scale_fac
             x2 = self.path[idx].x / tile_scale_fac
             y2 = self.path[idx].y / tile_scale_fac
-            canvas_id = self.canvas.create_line(x1, y1, x2, y2, fill="#8cf075")
+            canvas_id = self.canvas.create_line(x1, y1, x2, y2, fill=color, width=1.5)
             self.prev_line_id.append(canvas_id)
             idx += 1
-
 
 def staticGridSimulation():
     wMap = grid.Grid(tile_num_height, tile_num_width, tile_size)
