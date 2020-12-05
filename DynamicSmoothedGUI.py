c7460fc0172ff0bb3bbf91101db65a6ed803152f
Zoltan Csaki <zcc6@cornell.edu>
2020-12-05 14:46:50 -0500

    fix index out of bounds

--- DynamicSmoothedGUI.py
+++ DynamicSmoothedGUI.py
@@ -258,7 +258,7 @@ class DynamicGUI(GUI):
         self.canvas.pack()
 
     def checkBounds(self, x, y):
-        if x+1 > tile_size * tile_num_width or x < 0 or y+1 > tile_size * tile_num_height or y < 0:
+        if x+1 > tile_num_width or x < 0 or y+1 > tile_num_height or y < 0:
             return False
         elif self.gridEmpty.grid[y][x] in self.robot_tile_set:
             return False
