bfa8f989a3be9de8eccfd4aa680f5596428e44bd
Jacquelyn Peng <jtp224@cornell.edu>
2020-12-05 15:10:05 -0500

    add specs for MapMaker.py

--- MapMaker.py
+++ MapMaker.py
@@ -1,12 +1,18 @@
 
 
 def convertToMap(lens: list):
+    """Converts list lens, which contains integers, into their character equivalents and creates a
+    a string containing all those characters in order. This will be the content that goes into the
+    .txt file containing the seed for a custom map."""
     x = ""
     for i in lens:
         x+=chr(i)
     return x
 
 def main():
+    """Edit the contents of this function in order to create a custom map. Append integers representing each
+    character that you want in the custom seed onto list l. Then, this will convert this list of ints
+    into a string of characters representing the custom seed."""
     l = []
 
     for i in range(30):
