c8c1d835b7e0833a418a2d85dd07010e4efea337
Zoli <zcc6@cornelledu>
2021-02-28 09:40:00 -0500

    Got recieve data to work cleanly

--- ReceiveData.py
+++ ReceiveData.py
@@ -1,23 +1,20 @@
 import socket
 import pickle
 from Tile import *
-from tkinter import *
 
-## server file, 
+
+# server file,
 class ReceiveData:
-    host = '0.0.0.0'
-    def __init__(self, host):
-        self.host = host  # Server ip
+    def __init__(self):
         port = 4000
         self.s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.s.bind((host, port))
+        self.s.bind((self.get_ip(), port))
         print("Server Started")
 
-    
     def receive_data(self):
         buffer = []
         while True:
-            data, addr = self.s.recvfrom(4096)
+            data, addr = self.s.recvfrom(1024)
             # data equals None
             if not data:
                 break
@@ -25,10 +22,26 @@ class ReceiveData:
             # when a message has been received
             print("Message from: " + str(addr))
             buffer.append(data)
-            
+            if b"qqqqq" in data:
+                break
+        print(pickle.loads(b"".join(buffer)))
         return pickle.loads(b"".join(buffer))
 
-### TEST
+    def get_ip(self) -> str:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except Exception:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
+
+
+# TEST
 if __name__ == "__main__":
-    receiveData = ReceiveData("192.168.86.79")
-    print(receiveData.receive_data())
+    receiveData = ReceiveData()
+    while True:
+        receiveData.receive_data()
