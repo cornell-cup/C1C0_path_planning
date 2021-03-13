ad38c225a4ed70bb29858404ee1fa53be5c1d03f
Sanjit Basker <tijnasreksab@gmail.com>
2021-03-13 14:43:07 -0500

    more bug fixes

--- Server.py
+++ Server.py
@@ -8,28 +8,27 @@ class Server(Network):
         self.socket.bind((self.get_ip(), self.port))
         self.socket.settimeout(2)  # interferes with stopping
         print("Server Started")
-        self.client= ("", 0)
+        self.client = ("", 0)
 
     def receive_data(self):
         try:
-            x= self.socket.recvfrom(4096)
-            print(x[1:])
+            x = self.socket.recvfrom(4096)
+            self.client = x[1]
             return pickle.loads(x[0])
         except socket.timeout:
             return "no data within listening time"
 
-    ## precondition: must have called receive_data successfully
+    ##  precondition: must have called receive_data successfully
     def send_update(self, update):
         # TODO: implement
-        self.socket.sendto(1, self.client)
+        self.socket.sendto(pickle.dumps(update), self.client)
 
 
 # TEST
 if __name__ == "__main__":
     receiveData = Server()
     while True:
-        x= receiveData.receive_data()
-        if x!= "no data within listening time":
+        x = receiveData.receive_data()
+        if x != "no data within listening time":
             print(x)
             break
-
