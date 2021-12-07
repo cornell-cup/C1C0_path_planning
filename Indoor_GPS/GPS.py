0e9a55f54a3528ed0fb43f77de0ea9df603d5436
Zoli <zcc6@cornell.edu>
2021-12-07 16:31:10 -0500

    fix gps filtering

--- Indoor_GPS/GPS.py
+++ Indoor_GPS/GPS.py
@@ -8,7 +8,7 @@ class GPS():
         # create Marvel Mind Hedge thread
         # get USB port with ls /dev/tty.usb*
         # adr is the address of the hedgehog beacon!
-        self.hedge = MarvelmindHedge(tty=tty, adr=adr, debug=False)
+        self.hedge = MarvelmindHedge(tty=tty, adr=hedge_addr, debug=False)
         # start thread
         self.hedge.start()
         # REQUIRED SLEEP TIME in order for thread to start and init_pos to be correct
@@ -32,25 +32,29 @@ class GPS():
                 avgPosition[1] += prev_pos[1]
 
         if len(self.location_buffer) >= 5:
-            avgPosition[0] /= len(self.location_buffer)
-            avgPosition[1] /= len(self.location_buffer)
-            [_, y, x, z, ang, time] = self.hedge.position()
-            x = -x
-            x1 = x
-            y1 = y
-    
-            self.location_buffer.pop(0)
-            self.location_buffer.append((x1, y1))
-            # map the position to the correct frame of reference
-            x = (x - self.init_pos[1]) * 10
-            y = (y - self.init_pos[2]) * 10
-            x = int(tile_num_width/2) + int(x * 100 / tile_size)
-            y = int(tile_num_height/2) + int(y * 100 / tile_size)
-            prev_tile = curr_tile
-            curr_tile = self.grid.grid[x][y]
-            self.pid.update_PID(curr_tile.x, curr_tile.y)
+            try:
+                avgPosition[0] /= len(self.location_buffer)
+                avgPosition[1] /= len(self.location_buffer)
+                [_, y, x, z, ang, time] = self.hedge.position()
+                x = -x
+                x1 = x
+                y1 = y
 
-            return prev_tile, curr_tile
+                self.location_buffer.pop(0)
+                self.location_buffer.append((x1, y1))
+                # map the position to the correct frame of reference
+                x = (x - self.init_pos[1]) * 10
+                y = (y - self.init_pos[2]) * 10
+                x = int(tile_num_width/2) + int(x * 100 / tile_size)
+                y = int(tile_num_height/2) + int(y * 100 / tile_size)
+                prev_tile = curr_tile
+                curr_tile = self.grid.grid[x][y]
+                self.pid.update_PID(curr_tile.x, curr_tile.y)
+
+                return prev_tile, curr_tile
+            except:
+                print("GPS Reads robot position is off the screen!")
+                self.update_loc(curr_tile)
         else:
             print("IN GPS ELSE ")
             [_, y, x, z, ang, time] = self.hedge.position()
