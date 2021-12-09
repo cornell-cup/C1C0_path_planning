88f55457df8980910fc19aace87b0fbb479c86b7
Zoli <zcc6@cornell.edu>
2021-12-09 15:23:44 -0500

    Get rid of filtering

--- Indoor_GPS/GPS.py
+++ Indoor_GPS/GPS.py
@@ -25,42 +25,42 @@ class GPS():
         updates the current tile based on the GPS input
         """
         # call indoor gps get location function
-        avgPosition = [0, 0]
-        for prev_pos in self.location_buffer:
-            if prev_pos is not None:
-                avgPosition[0] += prev_pos[0]
-                avgPosition[1] += prev_pos[1]
+        # avgPosition = [0, 0]
+        # for prev_pos in self.location_buffer:
+        #     if prev_pos is not None:
+        #         avgPosition[0] += prev_pos[0]
+        #         avgPosition[1] += prev_pos[1]
 
-        if len(self.location_buffer) >= 5:
-            try:
-                avgPosition[0] /= len(self.location_buffer)
-                avgPosition[1] /= len(self.location_buffer)
-                [_, y, x, z, ang, time] = self.hedge.position()
-                x = -x
-                x1 = x
-                y1 = y
+        # if len(self.location_buffer) >= 5:
+        #     try:
+        #         avgPosition[0] /= len(self.location_buffer)
+        #         avgPosition[1] /= len(self.location_buffer)
+        [_, y, x, z, ang, time] = self.hedge.position()
+        x = -x
+        x1 = x
+        y1 = y
 
-                self.location_buffer.pop(0)
-                self.location_buffer.append((x1, y1))
-                # map the position to the correct frame of reference
-                x = (x - self.init_pos[1]) * 10
-                y = (y - self.init_pos[2]) * 10
-                x = int(tile_num_width/2) + int(x * 100 / tile_size)
-                y = int(tile_num_height/2) + int(y * 100 / tile_size)
-                prev_tile = curr_tile
-                curr_tile = self.grid.grid[x][y]
-                self.pid.update_PID(curr_tile.x, curr_tile.y)
+        self.location_buffer.pop(0)
+        self.location_buffer.append((x1, y1))
+        # map the position to the correct frame of reference
+        x = (x - self.init_pos[1]) * 10
+        y = (y - self.init_pos[2]) * 10
+        x = int(tile_num_width/2) + int(x * 100 / tile_size)
+        y = int(tile_num_height/2) + int(y * 100 / tile_size)
+        prev_tile = curr_tile
+        curr_tile = self.grid.grid[x][y]
+        self.pid.update_PID(curr_tile.x, curr_tile.y)
 
-                return prev_tile, curr_tile
-            except:
-                print("GPS Reads robot position is off the screen!")
-                self.update_loc(curr_tile)
-        else:
-            print("IN GPS ELSE ")
-            [_, y, x, z, ang, time] = self.hedge.position()
-            x = -x
-            x1 = x
-            y1 = y
-            self.location_buffer.append((x1, y1))
-
-            return self.update_loc(curr_tile)
+        return prev_tile, curr_tile
+        #     except:
+        #         print("GPS Reads robot position is off the screen!")
+        #         self.update_loc(curr_tile)
+        # else:
+        #     print("IN GPS ELSE ")
+        #     [_, y, x, z, ang, time] = self.hedge.position()
+        #     x = -x
+        #     x1 = x
+        #     y1 = y
+        #     self.location_buffer.append((x1, y1))
+        #
+        #     return self.update_loc(curr_tile)
