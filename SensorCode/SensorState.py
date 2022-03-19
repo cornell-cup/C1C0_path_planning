fd4cbe7c876df9d41783dd4330f17a5ae801c9ac
Angela Chao <angelachao345@gmail.com>
2022-03-19 14:52:10 -0400

    Json server-side testing with non-dummy data

--- SensorCode/SensorState.py
+++ SensorCode/SensorState.py
@@ -42,9 +42,10 @@ class SensorState:
         self.imu_count = 0
         self.heading_arr = [0] * 3
         self.heading = 0
-        if client:
-            TEST_API.init_serial('/dev/ttyTHS1', 115200) # port name may be changed depending on the machine
-            self.init_imu = self.get_init_imu()
+        self.init_imu = [0, 0, 0]
+        # if client:
+        #     TEST_API.init_serial('/dev/ttyTHS1', 115200) # port name may be changed depending on the machine
+        #     self.init_imu = self.get_init_imu()
 
 
     def package_data(self):
@@ -221,7 +222,7 @@ class SensorState:
         ans['heading_arr'] = self.heading_arr
         ans['imu_count'] = self.imu_count
         ans['heading'] = self.heading
-        # ans['init_imu'] = self.init_imu
+        ans['init_imu'] = self.init_imu
         return json.dumps(ans)
 
     """
--- ServerGUI.py
+++ ServerGUI.py
@@ -40,7 +40,7 @@ class ServerGUI:
         self.prev_draw_c1c0_ids = [None, None]
         self.create_widgets()
         self.server = input_server
-        self.processEndPoint(self.server.recieve_data_init()['end_point'])
+        self.processEndPoint(self.server.receive_data_init()['end_point'])
         #print('got the end point to be, ', self.endPoint)
         self.path = search.a_star_search(
             self.grid, (self.curr_tile.x, self.curr_tile.y), self.endPoint, search.euclidean)
@@ -224,7 +224,11 @@ class ServerGUI:
             motor_speed = self.computeMotorSpeed()
             self.server.send_update(motor_speed)
         #  TODO 2: Update environment based on sensor data
-        self.sensor_state = self.server.receive_data()
+        self.sensor_state = SensorState()
+        received_json = self.server.receive_data()
+        # print(received_json)
+        self.sensor_state.from_json(json.loads(received_json))
+        # print(self.sensor_state)
         self.update_grid_wrapper()
         self.visibilityDraw(self.filter_lidar(self.sensor_state.lidar))
 
